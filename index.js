9946eebace05748f11a0d15ffd439611afc1954f
GrzegorzNecka <strzypior@gmail.com>
2021-02-11 15:49:36 +0100

    2 files modified

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const members = [
 
 Vue.component("member-item", {
   props: ["member"],
-  template: `        <div class="flex justify-between px-2 py-2">
+  template: ` 
+  <div 
+    class="flex justify-between px-2 py-2">
           <p class="flex text-gray-700">
             <svg class="w-2 text-gray-500 mx-2" viewBox="0 0 8 8" fill="currentColor">
               <circle cx="4" cy="4" r="3" />
@@ -19,9 +21,29 @@ Vue.component("member-item", {
         </div>`
 });
 
+Vue.component("search-input", {
+  props: ["search"],
+  template: `
+  <input 
+    v-model="search" 
+    type="text" 
+    class="pl-8 p-1 bg-gray-200 w-full rounded relative" 
+    placeholder="Wyszukaj kontakt">`
+});
+
 new Vue({
   el: "#app",
   data: {
+    search: "",
     members
+  },
+  computed: {
+    filterMembers() {
+      return this.members.filter(member => {
+        return member.first_name
+          .toLowerCase()
+          .includes(this.search.toLowerCase());
+      });
+    }
   }
 });
